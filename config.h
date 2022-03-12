c7fdd5f55083c54e108bc72540e69acecd91b39c
png261 <nhphuong.code@gmail.com>
2022-03-12 21:50:43 +0700

    patched border

--- config.h
+++ config.h
@@ -14,10 +14,13 @@ static const char *colors[SchemeLast][2] = {
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 0;
+static unsigned int lines      = 20;
 
 /*
  * Characters not considered part of a word while deleting words
  * for example: " /?\"&[]"
  */
 static const char worddelimiters[] = " ";
+
+/* Size of the window border */
+static const unsigned int border_width = 5;
